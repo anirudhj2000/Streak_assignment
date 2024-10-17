2ecdc015ed42655950ec771320ff606a62a2148c
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-10-17 14:32:21 +0530

    go server changes

--- streak_assignment_be/go.mod
+++ streak_assignment_be/go.mod
@@ -2,4 +2,7 @@ module streak_assignment_be
 
 go 1.23.2
 
-require github.com/gorilla/mux v1.8.1 // indirect
+require (
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/rs/cors v1.11.1 // indirect
+)
--- streak_assignment_be/internal/server/server.go
+++ streak_assignment_be/internal/server/server.go
@@ -4,11 +4,22 @@ import (
 	"net/http"
 	"streak_assignment_be/internal/routes"
 	"streak_assignment_be/pkg/logger"
+
+	"github.com/rs/cors"
 )
 
 func Start() error {
 	r := routes.InitRouter()
 
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3000"}, // Allow frontend to access API
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
+		AllowedHeaders:   []string{"Authorization", "Content-Type"},
+		AllowCredentials: true,
+	})
+
+	handler := c.Handler(r)
+
 	logger.Info("Starting the server on port 8080")
-	return http.ListenAndServe(":8080", r)
+	return http.ListenAndServe(":8080", handler)
 }
