52c6d7f29c480ae3a3d1da145d6d38084fa24641
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-10-17 12:52:32 +0530

    landing page setup

---
+++ streak_assignment_ui/app/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PathFinder from "@/components/PathFinder";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center w-screen bg-white">
+      <PathFinder />
+    </main>
+  );
+}
