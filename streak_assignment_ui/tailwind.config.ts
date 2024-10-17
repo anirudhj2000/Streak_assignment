6dc268ab7d1be01001a892837bfb4e03fe7373ea
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-10-17 12:51:30 +0530

    tailwinf and readme changes

---
+++ streak_assignment_ui/tailwind.config.ts
@@ -0,0 +1,25 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        borderColor: "#c7c7c7",
+        selected: "#3ab34a",
+        active: "#cb6161",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
